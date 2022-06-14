0955b73f6f1ad83648662ae3ef48033fe0047524
sam4co <96746593+sam4co@users.noreply.github.com>
2022-06-14 16:00:15 +0100

    Update sleep_calculator.py

--- sleep_calculator.py
+++ sleep_calculator.py
@@ -1,21 +1,3 @@
-"""
-This is a program that i coded to give you the 4 best bedtime choices.
-
-Getting a good sleep at night is more than just going to bed early, it's about finding
-the right time to wake up feeling fresh and relaxed every morning.
-
-By waking up in one of these times your sleep cycles will be 90-minutes long; knowing that a good night's sleep 
-consists of 5-9 complete sleep cycles
-
-NB: Note that hours are in the 24 format!
-
-You have to enter the hour at which you want to wake up then the minute in separate lines
-"""
-
-
-
-
-
 """The hours are in te 24 format"""
 hours = range(00, 24)
 minutes = range(00, 61)
